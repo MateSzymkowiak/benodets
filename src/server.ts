102c5dda5d88fdb60f04e9bb5b9e7d4a30fd9b4e
mszymkowiak <mateusz.szymkowiak@l4a-soft.com>
2020-03-26 10:53:10 +0100

    prto reverse

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import App from './app';
 import PostsController from './posts/posts.controller';
 
-const port = process.env.PORT || 3000;
-
 const app = new App(
     [
         new PostsController(),
     ],
-    port
+    5000,
 );
 
 app.listen();
