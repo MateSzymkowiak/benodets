56527f190c753ea1922482b4c9cfb7e8727e5701
mszymkowiak <mateusz.szymkowiak@l4a-soft.com>
2020-03-26 10:42:55 +0100

    port

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import App from './app';
 import PostsController from './posts/posts.controller';
 
+const port = process.env.PORT || 3000;
+
 const app = new App(
     [
         new PostsController(),
     ],
-    3000,
+    port
 );
 
 app.listen();
